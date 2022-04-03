ceb3459e85a0848127567ab05f810403d962036c
Manal Ziadeh <manalhz@gmail.com>
2022-04-03 08:25:48 -0400

    Week 1 exercise

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App msg='thank you, friends' />
   </React.StrictMode>,
   document.getElementById('root')
 );
